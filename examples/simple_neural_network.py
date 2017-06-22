f82f8f1d71a494462ec48da14ddac3e2bc0bcbd6
Upul Bandara <upulbandara@gmail.com>
2017-06-22 06:58:18 -0500

    New package call datasets added

--- examples/simple_neural_network.py
+++ examples/simple_neural_network.py
@@ -1,27 +1,17 @@
 import numpy as np
 import aurora.autodiff as ad
 from aurora.optim import SGD
+import aurora.datasets as data
 import matplotlib.pyplot as plt
 import seaborn as sbn;
 
 sbn.set()
 
-np.random.seed(0)
-N = 100  # number of points per class
-D = 2  # dimensionality
-K = 3  # number of classes
+D = 2
 H = 150
-X_data = np.zeros((N * K, D))
-y_data = np.zeros(N * K, dtype='uint8')
-for j in range(K):
-    ix = range(N * j, N * (j + 1))
-    r = np.linspace(0.0, 1, N)
-    t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
-    X_data[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-    y_data[ix] = j
-
-y_one_hot = np.zeros((N * K, K))
-y_one_hot[range(N * K), y_data] = 1
+K = 3
+N = 100
+X_data, y_data, y_data_encoded = data.spiral(K, D, N, 0)
 
 X = ad.Variable(name="X")
 y = ad.Variable(name='y')
@@ -44,7 +34,7 @@ lr = 1e-3
 n_epoch = 1001
 optimizer = SGD(loss, params=[W1, b1, W2, b2], lr=lr, momentum=0.8)
 for i in range(n_epoch):
-    loss_now = optimizer.step(feed_dict={X: X_data, y: y_one_hot})
+    loss_now = optimizer.step(feed_dict={X: X_data, y: y_data_encoded})
     if i <= 10 or (i <= 100 and i % 10 == 0) or (i <= 1000 and i % 100 == 0):
         fmt_str = 'iter: {0:>5d} cost: {1:>8.5f}'
         print(fmt_str.format(i, loss_now[0]))
@@ -55,19 +45,3 @@ prob_values, = executor.run(feed_dict={X: X_data})
 
 correct = np.sum(np.equal(y_data, np.argmax(prob_values, axis=1)))
 print('Prediction accuracy: {0:>.3f}%'.format((correct / (N * K)) * 100.00))
-
-h = 0.01
-x_min, x_max = X_data[:, 0].min() - 1, X_data[:, 0].max() + 1
-y_min, y_max = X_data[:, 1].min() - 1, X_data[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                     np.arange(y_min, y_max, h))
-Z = np.stack((xx.ravel(), yy.ravel()), axis=1)
-prob_values, = executor.run(feed_dict={X: Z})
-Z = np.argmax(prob_values, axis=1)
-Z = Z.reshape(xx.shape)
-fig = plt.figure()
-plt.contourf(xx, yy, Z, cmap=plt.cm.PiYG, alpha=0.3, antialiased=True)
-plt.scatter(X_data[:, 0], X_data[:, 1], c=y_data, s=80, cmap=plt.cm.PiYG)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.show()
--- examples/softmax.py
+++ examples/softmax.py
@@ -1,57 +1,38 @@
 import numpy as np
 import aurora.autodiff as ad
-from aurora.optim import Adam, SGD
-import matplotlib.pyplot as plt
-import seaborn as sbn;
+import aurora.datasets as data
+from aurora.optim import SGD
 
-np.random.seed(0)
-N = 250  # number of points per class
-D = 2  # dimensionality
-K = 3  # number of classes
-X_data = np.zeros((N * K, D))
-y_data = np.zeros(N * K, dtype='uint8')
-for j in range(K):
-    ix = range(N * j, N * (j + 1))
-    r = np.linspace(0.0, 1, N)
-    t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * 0.2  # theta
-    X_data[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-    y_data[ix] = j
+D = 2
+H = 150
+K = 3
+N = 100
+X_data, y_data, y_data_encoded = data.spiral(K, D, N, 0)
 
-y_one_hot = np.zeros((N * K, K))
-y_one_hot[range(N * K), y_data] = 1
-
-######################################
 x = ad.Variable(name="x")
 y = ad.Variable(name='y')
-W = ad.Variable(name="w")
-b = ad.Variable(name="b")
+
+W = ad.Parameter(name="W", init=np.zeros((D, K)))
+b = ad.Parameter(name="b", init=np.zeros(K))
 
 z = ad.matmul(x, W)
-softmax = z + ad.broadcast_to(b, z)
+hid_1 = z + ad.broadcast_to(b, z)
+loss = ad.cross_entropy(hid_1, y)
 
-loss = ad.cross_entropy(softmax, y)
-w_val = np.zeros((D, K))
-b_val = np.zeros(K)
+n_epoch = 1000
+lr = 0.001
 
-n_epoch = 1500
-lr = 0.01
-
-optimizer = Adam(loss, optim_dict={W: w_val, b: b_val}, lr=lr)
-
-#grad_w, grad_b = ad.gradients(loss, [W, b])
-#executor = ad.Executor([loss, grad_w, grad_b])
+optimizer = SGD(loss, params=[W, b], lr=lr, momentum=0.9)
 
 for i in range(n_epoch):
-    step_params = optimizer.step(feed_dict={x: X_data, y: y_one_hot})
+    loss_now = optimizer.step(feed_dict={x: X_data, y: y_data_encoded})
     if i % 100 == 0:
-        fmt_str = 'iter: {0:>5d} cost: {1:>8.5f}, W: {2:>8.5f} b: {3:>8.5f}'
-        print(fmt_str.format(i,
-                             step_params[loss][0],
-                             step_params[W][0, 0],
-                             step_params[b][0]))
-softmax = ad.softmax(softmax)
-executor = ad.Executor([softmax])
-a, = executor.run(feed_dict={x: X_data, W: step_params[W], b: step_params[b]})
+        fmt_str = 'iter: {0:>5d} cost: {1:>8.5f}'
+        print(fmt_str.format(i, loss_now[0]))
 
-correct = np.sum(np.equal(y_data, np.argmax(a, axis=1)))
+prob = ad.softmax(hid_1)
+executor = ad.Executor([prob])
+prob_val, = executor.run(feed_dict={x: X_data})
+
+correct = np.sum(np.equal(y_data, np.argmax(prob_val, axis=1)))
 print('prediction accuracy: {}'.format(correct / (N * K)))
